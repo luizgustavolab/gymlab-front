911561a212b81bce28636b0a20b21ce734b9949e
Luiz Gustavo <luizgustavolab@gmail.com>
2026-06-01 14:44:09 -0300

    fix: banner lgdp config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -10,10 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly exibirBanner = signal(!localStorage.getItem('gymlab_lgpd_consent'));
-
-  protected computarConsentimento(aceitou: boolean): void {
-    localStorage.setItem('gymlab_lgpd_consent', JSON.stringify({ consentido: aceitou }));
-    this.exibirBanner.set(false);
-  }
+ 
 }
